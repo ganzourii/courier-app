1addc94971de0431a8b413d04eab0948c9b5b6d7
aganzouri <ganzouri97@gmail.com>
2024-03-16 17:55:17 +0200

    created and modified home page and shipment status

--- src/main/java/com/goods/courierapp/rest/ShipmentRestController.java
+++ src/main/java/com/goods/courierapp/rest/ShipmentRestController.java
@@ -1,14 +1,46 @@
 package com.goods.courierapp.rest;
 
+import com.goods.courierapp.models.Pilot;
+import com.goods.courierapp.models.Shipment;
+import com.goods.courierapp.service.ShipmentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
+@RequestMapping("/shipments")
 public class ShipmentRestController {
 
-    @GetMapping("/shipments/create")
+    private ShipmentService shipmentService;
+
+    @Autowired
+    public ShipmentRestController(ShipmentService theShipmentService) { this.shipmentService = theShipmentService;}
+
+    @GetMapping("/create")
     public String createShipment ()
     {
         return "create-shipment";
+    }
+
+    @GetMapping("/{trackNumber}")
+    public String getShipmentById (@RequestParam("trackNumber") int trackNumber, Model theModel)
+    {
+/*
+        Shipment theShipment = shipmentService.findById(trackNumber);
+
+        if(theShipment == null)
+        {
+            throw new RuntimeException(" There's no shipment with this Id: "+trackNumber );
+        }
+        else
+        {
+            theModel.addAttribute("shipment", theShipment);
+        }
+*/
+
+        return "shipment-status";
     }
 }
